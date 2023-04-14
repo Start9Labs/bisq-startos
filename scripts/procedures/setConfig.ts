c6f340428a36bc59f27e99a150f84ec77c39a295
Mariusz Kogen <k0gen@pm.me>
2023-04-14 20:08:10 +0200

    Fix bitcoind dependency

--- scripts/procedures/setConfig.ts
+++ scripts/procedures/setConfig.ts
@@ -1,3 +1,7 @@
-import { compat, } from "../deps.ts";
+import { compat, types as T } from "../deps.ts";
 
-export const setConfig = compat.setConfig;
+export const setConfig: T.ExpectedExports.setConfig = async (effects, input) => {
+  return await compat.setConfig(effects, input, {
+    bitcoind: []
+  });
+};
