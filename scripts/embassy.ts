baafef031203a798ab79f7ad8152f2e27f12d283
Mariusz Kogen <k0gen@pm.me>
2023-07-10 14:50:10 +0200

    remove properties Please enter the commit message for your changes. Lines starting

--- scripts/embassy.ts
+++ scripts/embassy.ts
@@ -1,6 +1,5 @@
 export { setConfig } from "./procedures/setConfig.ts";
 export { getConfig } from "./procedures/getConfig.ts";
-export { properties } from "./procedures/properties.ts";
 export { dependencies } from "./procedures/dependencies.ts";
 export { migration } from "./procedures/migrations.ts";
 export { health } from "./procedures/healthChecks.ts";
